9687526c4abaabac7ee38b042f172b9c1d1b022d
dforrester20549 <dforrester20549@ufide.ac.cr>
2023-02-13 23:57:02 -0600

    feat: Nuevo codigo de Pilas
    Se describe el código de Pilas

---
+++ Pilas/src/pilas/Pila_Creador.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pilas;
+
+/**
+ *
+ * @author fam.ForresterCalvo
+ */
+public class Pila_Creador {
+    private Node cima;
+    private int largo; 
+    
+}
